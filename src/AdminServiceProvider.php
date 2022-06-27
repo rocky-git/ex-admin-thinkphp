c6182938f760d6f073b7a47c9d9cf4aec2071185
rocky-git <631670747@qq.com>
2022-06-27 16:35:44 +0800

    支持更新ui版本

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -8,6 +8,7 @@ use ExAdmin\thinkphp\command\MigrateRun;
 use ExAdmin\thinkphp\command\PluginComposerCommand;
 use ExAdmin\thinkphp\command\SeedRun;
 use ExAdmin\ui\support\Container;
+use Symfony\Component\Filesystem\Filesystem;
 use think\Service;
 
 class AdminServiceProvider extends Service
@@ -22,8 +23,24 @@ class AdminServiceProvider extends Service
     {
 
         Container::getInstance()->plugin->register();
+        $this->updateVersion();
     }
-   
+    
+    protected function updateVersion(){
+        $file = public_path('ex-admin').DIRECTORY_SEPARATOR.'version';
+        $update = false;
+        if(!is_file($file)){
+            $update = true;
+        }
+        if(!$update && file_get_contents($file) != ex_admin_version()){
+            $update = true;
+        }
+        if($update){
+            $filesystem = new Filesystem();
+            $filesystem->mirror(dirname(__DIR__,2) . '/ex-admin-ui/resources',public_path('ex-admin'),null,['override'=>true]);
+            file_put_contents($file,ex_admin_version());
+        }
+    }
     /**
      * Register the application services.
      *
