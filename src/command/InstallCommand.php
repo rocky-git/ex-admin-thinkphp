6f525320533b359fb48f34fc967f0f53223e35f1
rocky-git <631670747@qq.com>
2022-09-15 23:25:18 +0800

    新增安装指定用户名密码

--- src/command/InstallCommand.php
+++ src/command/InstallCommand.php
@@ -10,6 +10,7 @@ use think\console\input\Argument;
 use think\console\input\Option;
 use think\console\Output;
 use think\facade\Console;
+use think\facade\Db;
 
 class InstallCommand extends Command
 {
@@ -20,14 +21,18 @@ class InstallCommand extends Command
             ->setDescription('Install the admin package');
         $this->addOption('force', 'f', Option::VALUE_NONE, 'Force overwrite file');
         $this->addOption('versions', null, Option::VALUE_REQUIRED, 'version number');
+        $this->addOption('username', null, Option::VALUE_REQUIRED, 'username');
+        $this->addOption('password', null, Option::VALUE_REQUIRED, 'password');
     }
 
     protected function execute(Input $input, Output $output)
     {
+
+
         $filesystem = new Filesystem;
         $filesystem->mirror(__DIR__ . '/../../../ex-admin-ui/resources',public_path('ex-admin'),null,['override'=>$input->hasOption('force')]);
        
-        $path = plugin()->download('thinkphp',$this->input->getOption('versions'));
+        $path = plugin()->download('thinkphp',$input->getOption('versions'));
         if ($path === false) {
             $this->output->warning('下载插件失败');
             return 0;
@@ -40,7 +45,18 @@ class InstallCommand extends Command
         unlink($path);
         plugin()->buildIde();
         plugin()->thinkphp->install();
+        $username = $input->getOption('username');
+        $password = $input->getOption('password');
+        if($username && $password){
+            $table = plugin()->thinkphp->config('database.user_table');
+            Db::name($table)->where('id',1)
+                ->update([
+                    'username' => $username,
+                    'password' => password_hash($password,PASSWORD_DEFAULT),
+                ]);
+        }
         $output->writeln(Console::call('plugin:composer',['thinkphp'])->fetch());
+        
         // 指令输出
         $output->writeln('install success');
     }
