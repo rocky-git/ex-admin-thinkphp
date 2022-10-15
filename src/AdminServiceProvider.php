1fec7f711683ea47e26472d0b1421d90f48a32f2
rocky <631670747@qq.com>
2022-10-15 22:10:51 +0800

    调整资源目录

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -27,7 +27,7 @@ class AdminServiceProvider extends Service
     }
     
     protected function updateVersion(){
-        $file = public_path('ex-admin').DIRECTORY_SEPARATOR.'version';
+        $file = public_path('exadmin').DIRECTORY_SEPARATOR.'version';
         $update = false;
         if(!is_file($file)){
             $update = true;
@@ -37,7 +37,7 @@ class AdminServiceProvider extends Service
         }
         if($update){
             $filesystem = new Filesystem();
-            $filesystem->mirror(dirname(__DIR__,2) . '/ex-admin-ui/resources',public_path('ex-admin'),null,['override'=>true]);
+            $filesystem->mirror(dirname(__DIR__,2) . '/ex-admin-ui/resources',public_path('exadmin'),null,['override'=>true]);
             file_put_contents($file,ex_admin_version());
         }
     }
