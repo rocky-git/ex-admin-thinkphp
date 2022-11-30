f40cdeb751c55c6dcccc478275b71809ba2f188a
rocky <631670747@qq.com>
2022-11-30 12:50:18 +0800

    支持依赖注入

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -25,6 +25,9 @@ class AdminServiceProvider extends Service
         set_error_handler([$this->app->make(Error::class), 'appError']);
         Container::getInstance()->plugin->register();
         $this->updateVersion();
+        \ExAdmin\ui\Route::setObjectParamAfter(function ($name){
+            return $this->app->make($name);
+        });
     }
     
     protected function updateVersion(){
